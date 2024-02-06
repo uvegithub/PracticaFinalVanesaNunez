454e3012aa357797b1924137100df0a6edc079f6
Vanesa Nunez <vanesanunez90dos@gmail.com>
2024-02-06 19:15:03 +0100

    actu 6.2

--- app/src/main/java/com/example/firebase/MainActivity.kt
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -16,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var intento: Intent
     lateinit var user_edit: TextInputEditText
     lateinit var contrasena_edit: TextInputEditText
+    lateinit var boton_registro: Button
+    lateinit var intento_registro: Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,15 +32,37 @@ class MainActivity : AppCompatActivity() {
         user_edit=findViewById(R.id.textinputedittextusuario)
         contrasena_edit=findViewById(R.id.textinputedittextcontrasena)
         boton_validar=findViewById(R.id.button)
+        boton_registro=findViewById(R.id.button_registrarse)
 
         boton_validar.setOnClickListener {
-            validacion()
+            validacion(user_layout, contrasena_layout, user_edit, contrasena_edit)
         }
 
         intento = Intent(this, MainActivity::class.java)
+        intento_registro = Intent(this, MainActivity::class.java)
+
+        boton_registro.setOnClickListener {
+            startActivity(intento_registro)
+        }
     }
 
-    fun validacion(){
+    fun validacion(usuario_layaout:TextInputLayout, contrasenalayout: TextInputLayout, usuario_edit:TextInputEditText, contrasenaedit:TextInputEditText){
         //admin: login:admin  contra:admin   usuario  login:user   contrasena: user
+        if(usuario_edit.text.toString()=="admin" || contrasenaedit.text.toString()=="admin"){
+            startActivity(intento)
+        }else if(usuario_edit.text.toString()=="user" || contrasenaedit.text.toString()=="user"){
+            startActivity(intento)
+        }else{
+            if(usuario_edit.text.toString()==""){
+                usuario_layaout.setError("Debe introducir el nombre de usuario")
+            }else if(contrasenaedit.text.toString()==""){
+                contrasenalayout.setError("Debe introducir la contrasena")
+            }else if(usuario_edit.text.toString()=="admin" || usuario_edit.text.toString()=="user"){
+                usuario_layaout.setError("Nombre de usuario incorrecto")
+            }else if(contrasenaedit.text.toString()=="admin"|| contrasenaedit.text.toString()=="user"){
+                contrasenalayout.setError("Contrasena incorrecta")
+            }
+        }
     }
+
 }
